1eb0b2437019f9764380c8a4f40b87cbd0cb16a1
Gianni Riccardi <riccardi.gianni@gmail.com>
2018-05-17 11:14:53 +0200

    Avancement Instrument

--- TP4Java/src/model/Instrument.java
+++ TP4Java/src/model/Instrument.java
@@ -5,10 +5,22 @@
  */
 package model;
 
+import java.util.*;
+
 /**
  *
  * @author riccardild
  */
 public class Instrument {
+    private List<Fonds> valFonds;
+    
+    public Instrument() {
+        this.valFonds = new ArrayList<>();
+    }
+    
+    public Instrument(ArrayList<Fonds> valFonds) {
+        this.valFonds = valFonds;
+    }
+    
     
 }
